b253eb192a9989d7370cb5c3cc5152f3df699942
jatomic <johnatomich@gmail.com>
2022-01-12 23:23:17 -0600

    Add routes for rooms 1 and 2

--- routes.py
+++ routes.py
@@ -4,3 +4,11 @@ from flask import render_template
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/room1')
+def room1():
+    pass
+
+@app.route('/room2')
+def room2():
+    pass
