00a167fb334c82cb44f85fd9d9cc3fa530295c3c
jatomic <johnatomich@gmail.com>
2022-01-20 22:03:22 -0600

    Fix spacing and formatting

--- routes.py
+++ routes.py
@@ -1,13 +1,16 @@
 from app import app
 from flask import render_template
 
+
 @app.route('/')
 def index():
     return render_template("index.html")
 
+
 @app.route('/room1')
 def room1():
     pass
+
 
 @app.route('/room2')
 def room2():
