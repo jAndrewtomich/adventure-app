2404fec6e86f820f84fa9fab4946d2d738e77f23
jatomic <johnatomich@gmail.com>
2022-01-26 15:48:32 -0600

    Rename appropriately to avoid confusion with app module

---
+++ adventure.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask, render_template, url_for
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
+
+#This is meant to be a game modeled after a choose-your-own adventure book.
